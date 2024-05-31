eaead8e0317a8c2280f400a5a7edecd224f93e9b
AthkiaAdiba <athkiaadiba@gmail.com>
2024-05-31 21:58:23 +0600

    added authprovider  related

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,121 @@
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+import auth from "../firebase/firebase.config";
+
+
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+   const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+
+    // Providers
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+
+
+    // create User
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // Login user
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // login with google
+    const loginWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    // login with Github
+    const loginWithGithub = () => {
+        setLoading(true);
+        return signInWithPopup(auth, githubProvider);
+    }
+
+    // Update profile
+    const updateInformation = (userInformation, name, image) => {
+        return updateProfile(userInformation, {
+            displayName: name,
+            photoURL: image
+        })
+    }
+
+    // Log out
+    const logOut = () => {
+        setLoading(true);
+        signOut(auth)
+    }
+
+    // observer
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+    // subscribe user
+    // useEffect(() => {
+    //     const unsubscribe = onAuthStateChanged(auth, currentUser => {
+    //         setUser(currentUser);
+    //         // console.log('current user', currentUser);
+    //         if (currentUser) {
+    //             // get token and store client
+    //             const userInfo = { email: currentUser.email }
+    //             axiosPublic.post('/jwt', userInfo)
+    //                 .then(res => {
+    //                     if (res.data.token) {
+    //                         localStorage.setItem('access-token', res.data.token)
+    //                         setLoading(false);
+    //                     }
+    //                 })
+    //         }
+    //         else {
+    //             // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
+    //             localStorage.removeItem('access-token')
+    //             setLoading(false);
+    //         }
+
+    //     })
+    //     return () => {
+    //         return unsubscribe();
+    //     }
+    // }, [axiosPublic])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        updateInformation,
+        logOut,
+        login,
+        loginWithGoogle,
+        loginWithGithub
+    };
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+AuthProvider.propTypes = {
+    children: PropTypes.node
+}
+
+export default AuthProvider;
