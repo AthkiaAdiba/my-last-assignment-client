94536bb912bb40c0f87273607811543f870bedf6
AthkiaAdiba <athkiaadiba@gmail.com>
2024-06-02 08:32:58 +0600

    created useAxiosSecure hook

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -3,6 +3,7 @@ import { createContext, useEffect, useState } from "react";
 import PropTypes from 'prop-types';
 import { getAuth } from "firebase/auth";
 import app from '../firebase/firebase.config'
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 
 
@@ -10,6 +11,7 @@ export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+    const axiosPublic = useAxiosPublic();
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
@@ -58,44 +60,43 @@ const AuthProvider = ({ children }) => {
     }
 
     // observer
-    useEffect(() => {
-        const unSubscribe = onAuthStateChanged(auth, currentUser => {
-            console.log(currentUser);
-            setUser(currentUser);
-            setLoading(false);
-        });
-        return () => {
-            unSubscribe();
-        }
-    }, [])
+    // useEffect(() => {
+    //     const unSubscribe = onAuthStateChanged(auth, currentUser => {
+    //         console.log(currentUser);
+    //         setUser(currentUser);
+    //         setLoading(false);
+    //     });
+    //     return () => {
+    //         unSubscribe();
+    //     }
+    // }, [])
 
     // subscribe user
-    // useEffect(() => {
-    //     const unsubscribe = onAuthStateChanged(auth, currentUser => {
-    //         setUser(currentUser);
-    //         // console.log('current user', currentUser);
-    //         if (currentUser) {
-    //             // get token and store client
-    //             const userInfo = { email: currentUser.email }
-    //             axiosPublic.post('/jwt', userInfo)
-    //                 .then(res => {
-    //                     if (res.data.token) {
-    //                         localStorage.setItem('access-token', res.data.token)
-    //                         setLoading(false);
-    //                     }
-    //                 })
-    //         }
-    //         else {
-    //             // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
-    //             localStorage.removeItem('access-token')
-    //             setLoading(false);
-    //         }
-
-    //     })
-    //     return () => {
-    //         return unsubscribe();
-    //     }
-    // }, [axiosPublic])
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            // console.log('current user', currentUser);
+            if (currentUser) {
+                // get token and store client
+                const userInfo = { email: currentUser.email }
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token)
+                            setLoading(false);
+                        }
+                    })
+            }
+            else {
+                // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
+                localStorage.removeItem('access-token')
+                setLoading(false);
+            }
+        })
+        return () => {
+            return unsubscribe();
+        }
+    }, [axiosPublic])
 
     const authInfo = {
         user,
