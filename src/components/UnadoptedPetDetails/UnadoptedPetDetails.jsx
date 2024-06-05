cc88579b3b2b0a173bb746c61b73b7ede78d575e
AthkiaAdiba <athkiaadiba@gmail.com>
2024-06-05 08:05:38 +0600

    added unadopted pet details

--- src/components/UnadoptedPetDetails/UnadoptedPetDetails.jsx
+++ src/components/UnadoptedPetDetails/UnadoptedPetDetails.jsx
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import ModalForm from "../ModalForm/ModalForm";
 
 
 const UnadoptedPetDetails = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const axiosSecure = useAxiosSecure();
 
     const { data: pet = {} } = useQuery({
@@ -15,6 +16,10 @@ const UnadoptedPetDetails = () => {
         }
     })
     console.log(pet)
+
+    const handleOpenModal = () => {
+        document.getElementById('my_modal_1').showModal()
+    }
     return (
         <div className="px-[3%] lg:px-[25%] pt-28 lg:pt-36 pb-24">
             <div className="card bg-base-100 shadow-xl">
@@ -27,7 +32,20 @@ const UnadoptedPetDetails = () => {
                     <p className="text-2xl">Location: {pet.pet_location}</p>
                     <p className="text-2xl">Category: {pet.pet_category}</p>
                     <div className="card-actions justify-end">
-                    <button className="btn bg-[#FF720F] text-white">Adopt</button>
+                        {/* Open the modal using document.getElementById('ID').showModal() method */}
+                        <button onClick={handleOpenModal} className="btn bg-[#FF720F] text-white">Adopt</button>
+                        <dialog id="my_modal_1" className="modal">
+                            <div className="modal-box">
+                                <h2 className="mb-3 text-center text-4xl font-bold text-[#FF720F]">{pet.pet_name}</h2>
+                                <ModalForm></ModalForm>
+                                <div className="modal-action">
+                                    <form method="dialog">
+                                        {/* if there is a button in form, it will close the modal */}
+                                        <button className="btn bg-[#FF720F] text-white">Close</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </dialog>
                     </div>
                 </div>
             </div>
