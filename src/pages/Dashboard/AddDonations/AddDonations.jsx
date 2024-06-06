59dfe67e63184762efbff354917bfdd819ff30cf
AthkiaAdiba <athkiaadiba@gmail.com>
2024-06-06 08:48:13 +0600

    saving donation campaigns

--- src/pages/Dashboard/AddDonations/AddDonations.jsx
+++ src/pages/Dashboard/AddDonations/AddDonations.jsx
@@ -1,52 +1,71 @@
 import Swal from "sweetalert2";
 import useAuth from "../../../hooks/useAuth";
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
+import { useState } from "react";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddDonations = () => {
     const { user } = useAuth();
+    const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
+    const [startDate, setStartDate] = useState(new Date());
+    const [startDate2, setStartDate2] = useState(new Date());
 
 
-    const handleAddDonations = e => {
+    const handleAddDonations = async (e) => {
         e.preventDefault();
 
         const form = e.target;
 
-        const countryName = form.countryName.value;
-        const spotName = form.touristsSpotName.value;
-        const location = form.location.value;
-        const cost = form.cost.value;
-        const season = form.season.value;
-        const photo = form.photo.value;
-        const visitors = form.visitors.value;
-        const travelTime = form.travelTime.value;
-        const description = form.description.value;
-
-        const addSpot = { countryName, spotName, location, cost, season, photo, travelTime, visitors, description, userEmail: user.email, userName: user.displayName }
-        // console.log(addSpot);
-
-        fetch('https://my-tenth-project-server.vercel.app/addSpot', {
-            method: 'POST',
+        
+        // image upload to imgbb and then get an url
+        const imageFile = { image: form.image.files[0] }
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
             headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(addSpot)
+                'content-type': 'multipart/form-data'
+            }
         })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (data.insertedId) {
-                    Swal.fire({
-                        title: 'Success!',
-                        text: 'Tourists Spot Added Successfully',
-                        icon: 'success',
-                        confirmButtonText: 'Cool'
-                    })
-                }
+        // console.log('with image url', res.data.data.display_url)
+        if (res.data.success) {
+            // now send the campaign to the server with the image
+            const campaign = {
+                pet_name: form.petName.value,
+                maximumAmount: form.maximumAmount.value,
+                pet_image: res.data.data.display_url,
+                create_date: startDate,
+                last_date: startDate2,
+                short_description: form.shortDescription.value,
+                long_description: form.longDescription.value,
+                user_name: user.displayName,
+                email: user.email
+            }
+            console.log(campaign)
+            // post a campaign
+            const campaignRes = await axiosSecure.post('/campaign', campaign)
+            console.log(res.data)
+            if (campaignRes.data.insertedId) {
+                // show success popup
                 form.reset();
-            })
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${form.petName.value} is added to the campaigns.`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        }
+        // console.log('with image url', res.data)
+
     }
     return (
-        <div className="font-barlow mb-24 py-16 px-3 lg:px-24 mt-0 lg:mt-20">
+        <div className="font-barlow mb-10 lg:mb-24 py-16 px-3 lg:px-24 mt-0 lg:mt-20">
             <h2 className="text-4xl font-extrabold text-center mb-5 text-black">Add A Campaign</h2>
             <form onSubmit={handleAddDonations}>
                 {/* pet Name and maximum account row */}
@@ -56,7 +75,7 @@ const AddDonations = () => {
                             <span className="label-text text-black text-xl font-medium">Pet Name</span>
                         </label>
                         <label className="input-group text-black">
-                            <input type="text" name="petName" placeholder="Pet Name" className="input input-bordered w-full" />
+                            <input type="text" name="petName" placeholder="Pet Name" className="input input-bordered w-full" required />
                         </label>
                     </div>
                     <div className="form-control md:w-1/2 ml-0 md:ml-4 lg:ml-4">
@@ -68,68 +87,28 @@ const AddDonations = () => {
                         </label>
                     </div>
                 </div>
-                {/* location and average cost row */}
+                {/* donation create and last date row */}
                 <div className="md:flex mb-3 lg:mb-8">
                     <div className="form-control md:w-1/2 mb-3 lg:mb-0">
                         <label className="label">
-                            <span className="label-text text-white text-xl font-medium">Location</span>
+                            <span className="label-text text-black text-xl font-medium">Donation Create Date</span>
                         </label>
-                        <label className="input-group">
-                            <input type="text" name="location" placeholder="Location" className="input input-bordered w-full" required />
-                        </label>
+                        <DatePicker className="w-full ml-1 p-2 border-2 rounded-md text-black text-xl"
+                            selected={startDate} onChange={(date) => setStartDate(date)} required />
                     </div>
                     <div className="form-control md:w-1/2 ml-0 md:ml-4 lg:ml-4">
                         <label className="label">
-                            <span className="label-text text-white text-xl font-medium">Average Cost</span>
+                            <span className="label-text text-black text-xl font-medium">Donation Last Date</span>
                         </label>
-                        <label className="input-group">
-                            <input type="text" name="cost" placeholder="Average Cost" className="input input-bordered w-full" required />
-                        </label>
-                    </div>
-                </div>
-                {/* season and travel time row */}
-                <div className="md:flex mb-3 lg:mb-8">
-                    <div className="form-control md:w-1/2 mb-3 lg:mb-0">
-                        <label className="label">
-                            <span className="label-text text-white text-xl font-medium">Season</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="season" placeholder="Season" className="input input-bordered w-full" required />
-                        </label>
-                    </div>
-                    <div className="form-control md:w-1/2 ml-0 md:ml-4 lg:ml-4">
-                        <label className="label">
-                            <span className="label-text text-white text-xl font-medium">Travel Time</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="travelTime" placeholder="Travel Time" className="input input-bordered w-full" required />
-                        </label>
-                    </div>
-                </div>
-                {/* image URL and totalVisitorsPerYear row */}
-                <div className="md:flex mb-3 lg:mb-8">
-                    <div className="form-control md:w-1/2 mb-3 lg:mb-0">
-                        <label className="label">
-                            <span className="label-text text-white text-xl font-medium">Photo URL</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered w-full" required />
-                        </label>
-                    </div>
-                    <div className="form-control md:w-1/2 ml-0 md:ml-4 lg:ml-4">
-                        <label className="label">
-                            <span className="label-text text-white text-xl font-medium">Total Visitors Per Year</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="visitors" placeholder="Total Visitors Per Year" className="input input-bordered w-full" required />
-                        </label>
+                        <DatePicker className="w-full ml-1 p-2 border-2 rounded-md text-black text-xl"
+                            selected={startDate2} onChange={(date) => setStartDate2(date)} required />
                     </div>
                 </div>
                 {/* user name and email row */}
                 <div className="md:flex mb-3 lg:mb-8">
                     <div className="form-control md:w-1/2 mb-3 lg:mb-0">
                         <label className="label">
-                            <span className="label-text text-white text-xl font-medium">User Email</span>
+                            <span className="label-text text-black text-xl font-medium">User Email</span>
                         </label>
                         <label className="input-group">
                             <input type="email" name="email" defaultValue={user?.email} disabled placeholder="User Email" className="input input-bordered w-full" required />
@@ -137,7 +116,7 @@ const AddDonations = () => {
                     </div>
                     <div className="form-control md:w-1/2 ml-0 md:ml-4 lg:ml-4">
                         <label className="label">
-                            <span className="label-text text-white text-xl font-medium">User Name</span>
+                            <span className="label-text text-black text-xl font-medium">User Name</span>
                         </label>
                         <label className="input-group">
                             <input type="text" name="userName" defaultValue={user?.displayName} disabled placeholder="User Name" className="input input-bordered w-full" required />
@@ -148,22 +127,30 @@ const AddDonations = () => {
                 <div className="md:flex mb-3 lg:mb-8">
                     <div className="form-control md:w-1/2 ml-0 md:ml-4 lg:ml-4">
                         <label className="label">
-                            <span className="label-text text-white text-xl font-medium">Short Description</span>
+                            <span className="label-text text-black text-xl font-medium">Short Description</span>
                         </label>
                         <label className="input-group">
-                            <textarea name="description" placeholder="Short Description" className="textarea textarea-bordered textarea-sm w-full" required ></textarea>
+                            <textarea name="shortDescription" placeholder="Short Description" className="textarea textarea-bordered textarea-sm w-full" required ></textarea>
                         </label>
                     </div>
                     <div className="form-control md:w-1/2 ml-0 md:ml-4 lg:ml-4">
                         <label className="label">
-                            <span className="label-text text-white text-xl font-medium">Short Description</span>
+                            <span className="label-text text-black text-xl font-medium">Long Description</span>
                         </label>
                         <label className="input-group">
-                            <textarea name="description" placeholder="Short Description" className="textarea textarea-bordered textarea-sm w-full" required ></textarea>
+                            <textarea name="longDescription" placeholder="Long Description" className="textarea textarea-bordered textarea-sm w-full" required ></textarea>
                         </label>
                     </div>
                 </div>
-                <input type="submit" value="Add Tourists Spot" className="btn bg-[#0f2454] text-white text-xl font-medium border-none w-full" />
+                <div className="form-control w-full lg:w-1/3 mb-4 lg:mb-8">
+                    <label className="label">
+                        <span className="label-text text-xl font-medium">Pet Image</span>
+                    </label>
+                    <label className="input-group">
+                        <input type="file" name="image" className="file-input file-input-bordered w-full" required />
+                    </label>
+                </div>
+                <input type="submit" value="Add A Capaign" className="btn bg-[#FF720F] text-white text-xl font-medium border-none w-full" />
 
             </form>
         </div>
